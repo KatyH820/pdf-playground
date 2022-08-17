9b44c9eea150e9531df5ab9a305127b527d0149c
Katy Huang <Katyh1@uci.edu>
2022-08-17 15:51:02 -0700

    changing comment

--- pdf.py
+++ pdf.py
@@ -5,7 +5,7 @@ with open('dummy.pdf', 'rb') as file:  # use rb (read binary) when working with 
     print(reader.numPages)  # number page
     page = reader.getPage(0)  # page object
     page.rotateCounterClockwise(90)
-    # update our dummy.pdf to actually write and rotate counterclockwise
+    # update our dummy.pdf to actually write and rotate counterclockwise.
     writer = PyPDF2.PdfFileWriter()
     writer.addPage(page)
     with open('tilt.pdf', 'wb') as new_file:
